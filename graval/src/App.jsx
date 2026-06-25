07d8b300c96406bf2a7f795629b8940a905a3a64
aldo <aldojalloquispe@gmail.com>
2026-06-25 11:58:52 -0500

    desplegando carrusel de fotos y bienvenida

--- graval/src/App.jsx
+++ graval/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import Body from "./components/body/Body";
 import Footer from "./components/footer/Footer";  
+import HeroCarousel from "./components/herocarousel/HeroCarousel";
 function App() {
 
   return (
     <>
     <Navbar />
+    <HeroCarousel />
     <Body />
     <Footer />  
     </>
