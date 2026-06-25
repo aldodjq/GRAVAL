e3123b80804693f84d7ff415f1a5aee7132cb223
aldo <aldojalloquispe@gmail.com>
2026-06-25 09:38:25 -0500

    probando navbar nuevo

--- graval/src/components/navbar/Navbar.jsx
+++ graval/src/components/navbar/Navbar.jsx
@@ -1,12 +1,28 @@
+import React from "react";
+import "./Navbar.css";
+
 function Navbar() {
   return (
-    <nav>
-      <h1>My App</h1>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+    <nav class="navbar">
+        <div class="logo">
+            <img src="logo.png" alt="GRAVAL">
+            <span>GRAVAL</span>
+        </div>
+
+        <ul class="nav-links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Proyectos</a></li>
+            <li><a href="#">Clientes</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+
+        <div class="nav-actions">
+            <button class="btn-cotizacion">
+                Solicitar Cotización
+            </button>
+        </div>
     </nav>
   );
 }
