e8b9cc27d1b4cc1301464df82b98edd7a3254b24
aldo <aldojalloquispe@gmail.com>
2026-07-09 23:25:25 -0500

    combinando componentes

--- graval/src/pages/Home.jsx
+++ graval/src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import Hero from "../components/hero/Hero";
+import Proyectos from "../components/proyectos/Proyectos";
+import Servicios from "../components/servicios/Servicios";
+import Team from "../components/team/Team";
+import Testimonios from "../components/testimonios/Testimonios";
+
 // src/pages/Home.jsx
 export function Component() {
   return (
     <section style={{ padding: "80px 24px" }}>
-      <h1>Inicio</h1>
-      <p>Bienvenido a GRAVAL Constructora.</p>
+      <Hero/>
+      <Servicios/>
+      <Testimonios/>
+      <Team/>
+      <Proyectos/>
     </section>
   );
 }
